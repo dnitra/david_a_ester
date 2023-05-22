7902d4743370094d196f8297797898b4ae3938a7
dnitra <109307365+dnitra@users.noreply.github.com>
2023-05-22 13:08:36 +0200

    added burger menu for mobile and fixed map

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-// hide nav on scroll down
-const headerNav = document.querySelector(".header-nav");
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    headerNav.style.top = "0";
+// JavaScript to toggle the menu
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
+
+burger.addEventListener("click", () => {
+  menu.classList.toggle("menu-open");
+});
+
+let prevScrollPos = window.pageYOffset;
+
+window.addEventListener("scroll", () => {
+  const currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos > currentScrollPos) {
+    // Scrolling up
+    document.querySelector(".header-nav").style.top = "0";
   } else {
-    headerNav.style.top = "-100px";
+    // Scrolling down
+    document.querySelector(".header-nav").style.top = "-50px";
   }
-  prevScrollpos = currentScrollPos;
-};
+
+  prevScrollPos = currentScrollPos;
+});
+
+// Close the menu when a link is clicked
+const menuLinks = document.querySelectorAll(".menu a");
+
+menuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        menu.classList.toggle("menu-open");
+    });
+
+     document.querySelector(".header-nav").style.top = "-50px";
+}
+);
+
